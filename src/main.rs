68002d9c41fd0d1b559e8c7bc3a5a3c86d19dabe
Joel Bradshaw <cincodenada@gmail.com>
2021-11-18 08:54:46 -0800

    Add a very sketched-out bracketed-chunk iterator
    This is all in very non-Rust-y psuedocode, it'll be fun to transform it

--- src/main.rs
+++ src/main.rs
@@ -188,6 +188,59 @@ fn dbgIter<I, T>(it: I) -> impl Iterator<Item=T> where I: Iterator<Item=T>, T: s
     collected.into_iter()
 }
 
+struct BracketedChunks<I> where I: Iterator,
+{
+    min: I::Item,
+    max: I::Item,
+    size: I::Item,
+    prev: I::Item,
+    source: I,
+}
+
+impl<I> Iterator for BracketedChunks<I>
+where
+    I: Iterator
+{
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if(!prev) {
+            while let prev = source.next() if prev < min {}
+            return min
+        }
+        if(prev == max) {
+            prev = None
+            return max
+        }
+        if(prev) {
+            let cur = prev
+            prev = source.next();
+            if(!prev) { prev = max }
+            // TODO: Make check for > case = bad
+            if((cur/size).floor() < (prev/size).floor()) {
+                return (cur/size).floor()
+            } else {
+                return cur
+            }
+        } else {
+            return None
+        }
+    }
+}
+
+trait Bracketed: Iterator {
+    fn bracketed_chunks<T>(self, min: T, max: T) -> BracketedChunks<Self>
+    where Self::Item: T
+    {
+        BracketedChunks {
+            seen: HashSet::new(),
+            underlying: self,
+        }
+    }
+}
+
+impl<I: Iterator> Bracketed for I {}
+
 fn make_rectangles(mag: &[f32], max_freq: u32, clip: (f32, f32)) -> (Vec<(f32, [[f64;2];4])>, f32, f32) {
     let clip_ord = clip.map(OrderedFloat);
     let freqs = mag.iter().enumerate()
